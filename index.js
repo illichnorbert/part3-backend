cf9828d237821504b63b31758282160f8f0a4b1b
Norbert Illich <illichnorbert@gmail.com>
2026-06-16 17:37:27 +0200

    Add frontend production build

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan')
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cors())
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
